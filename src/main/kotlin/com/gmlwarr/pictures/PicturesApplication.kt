c9d1afecb7efff40d91cf4dc2bc17e0168603fb0
gmlwarr <gmlima@gmail.com>
2024-02-18 22:37:22 -0300

    feat:Initial commit

---
+++ src/main/kotlin/com/gmlwarr/pictures/PicturesApplication.kt
@@ -0,0 +1,11 @@
+package com.gmlwarr.pictures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PicturesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PicturesApplication>(*args)
+}
